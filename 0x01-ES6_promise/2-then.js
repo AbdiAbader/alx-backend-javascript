0223987841513c35404a234fd1a031ebf3d269ab
AbdiAbader <abdiprocode@gmail.com>
2023-03-22 21:48:14 +0300

    updated taskes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((response) => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }).catch((error) => {
-    console.error(error);
-    return new Error();
-  });
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
