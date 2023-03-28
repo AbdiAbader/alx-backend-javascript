c04f6ca60614b0eb72dd9137b4e97f829b0dd922
AbdiAbader <abdiprocode@gmail.com>
2023-03-28 14:35:33 +0300

    advanced tasks

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const { protocol, name } = endpoint;
+  const key = `${protocol}_${name}`;
+
+  const count = weakMap.get(key) || 0;
+  if (count >= 5) {
+    throw new Error('Endpoint load is high.');
+  }
+  weakMap.set(key, count + 1);
+
+    return weakMap.get(key);
+}
