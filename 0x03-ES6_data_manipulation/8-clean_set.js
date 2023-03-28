c278d0efd156d44aa19fa60f6ee709aff4f5e701
AbdiAbader <abdiprocode@gmail.com>
2023-03-28 14:02:16 +0300

    task 8 updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
   const myarray = Array.from(set);
-  if (startString === '') {
+  if (startString === '' || startString === undefined
+  || startString === null || startString === NaN || typeof startString !== 'string') {
     return '';
   }
   const filteredSet = myarray.filter((item) => item.startsWith(startString));
