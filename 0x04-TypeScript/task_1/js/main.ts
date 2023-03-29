99c1b0253adaf4dabd96740d2c979c9b3b3ab89f
AbdiAbader <abdiprocode@gmail.com>
2023-03-29 19:41:04 +0300

    add task 2 and 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,4 +10,26 @@ interface Teacher {
   interface Directors extends Teacher {
     numberOfReports: number;
     }
-    
+
+const printTeacher = (firstName: string, lastName: string) => {
+    console.log(`${firstName[0]}. ${lastName}`);
+  }
+  interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+    }
+class StudentClass {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public lastName: string
+    )
+    {}
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return this.fullName;
+    }
+
+}
