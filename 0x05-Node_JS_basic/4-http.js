c58e2c245458ac71e155aded34699de2551f80a4
AbdiAbader <abdiprocode@gmail.com>
2023-05-16 23:03:42 +0300

    task 6

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,7 @@
 const http = require('http');
 
 const route = http.createServer((req, res) => {
-    res.end('Hello Holberton School!');
-    }
-    );
+  res.end('Hello Holberton School!');
+});
 route.listen(1245);
 module.exports = route;
