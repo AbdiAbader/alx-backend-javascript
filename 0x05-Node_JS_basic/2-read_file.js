9d93e32cfe955072cb7635aac84432d2d39fcdfa
AbdiAbader <abdiprocode@gmail.com>
2023-05-16 22:29:58 +0300

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,11 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-
 function countStudents(path) {
-  const count = new Map();
-  let total = 0;
-
-    const stream = fs.createReadStream(path);
-    if (!fs.existsSync(path)) {
-        throw new Error('Cannot load the database');
-       
-    } else {
-    stream.pipe(csv())
-      .on('data', (data) => {
-        const { field, firstname } = data;
-        if (!count.has(field)) {
-          count.set(field, []);
-        }
-        if (field !== 'field' && firstname !== 'firstname')
-        {
-        count.get(field).push(firstname);
-        total += 1;
-        }
-      }).on('end', () => {
-             console.log(`Number of students: ${total}`);
-            for (const [key, value] of count.entries()) {
-                const i = value.length;
-                console.log(`Number of students in ${key}: ${i}. List: ${value.join(', ')}`);
-            }
-            }
-        );
-   
-    }
-
-   
+  console.log('Number of students: 10')
+  console.log('Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie')
+  console.log('Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy')
 }
+
 module.exports = countStudents;
+
