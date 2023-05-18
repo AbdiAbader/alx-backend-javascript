f17918390e86c911171dc5463675b980ceb4337f
AbdiAbader <abdiprocode@gmail.com>
2023-05-18 16:32:04 +0300

    make assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,31 @@
-const { expect } = require('chai');
+const assert = require('assert');
 const calcul = require('./0-calcul');
 
 describe('calcul', () => {
   it('should return incorrect input', () => {
-    expect(() => calcul('')).to.throw();
-  });
+   assert.throws(() => calcul('a', 1), {
+        name: 'TypeError',
+        message: 'Parameters must be numbers'
 
+  }
+   );
+  assert.throws(() => calcul(1, 'a'), {
+        name: 'TypeError',
+        message: 'Parameters must be numbers'
+  
+  });});
   it('should return correct result', () => {
-    expect(calcul(1, 1)).to.equal(2);
+   assert.strictEqual(calcul(1, 3), 4);
+   assert.strictEqual(calcul(100, 3.7), 104);
+    assert.strictEqual(calcul(1000, 3.7), 1004);
+    assert.strictEqual(calcul(1.1, 3.7), 5);
   });
 
   it('should throw a TypeError', () => {
-    expect(() => calcul('a', 1)).to.throw(TypeError, 'Parameters must be numbers');
-  });
-});
+    assert.throws(() => calcul(''), {
+        name: 'TypeError'
+
+    });
+    });
+    });
+    
