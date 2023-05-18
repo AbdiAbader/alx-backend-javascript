9e22d599723994db1048835c80956e0950846db1
AbdiAbader <abdiprocode@gmail.com>
2023-05-18 22:06:19 +0300

    task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,17 +4,28 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
     it('should call calculateNumber', () => {
         const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
      expect(sendPaymentRequestToApi(100, 20)).to.equal('The total is: 10');
         expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-        calculateNumberStub.restore();
+        
     });
     });
     it('should call calculateNumber 2', () => {
  const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
   expect(sendPaymentRequestToApi(10, 10)).to.equal('The total is: 20');
   expect(calculateNumberStub.calledOnceWithExactly('SUM', 10, 10)).to.be.true;   
-  calculateNumberStub.restore();
+ 
 }
 );
