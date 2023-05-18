c2f4636a793088c8fb7be75647c6b879dfc3b6c2
AbdiAbader <abdiprocode@gmail.com>
2023-05-18 21:13:36 +0300

    task 3

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,8 @@
+const Utils = require('./utils');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  total = Utils.calculateNumber('SUM', a = totalAmount, b = totalShipping);
+  console.log(`The total is: ${total}`);
+
+}
+
+module.exports = sendPaymentRequestToApi;
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,29 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+          throw TypeError('Parameters must be numbers')
+        }
+        if (isNaN(a) || isNaN(b)) {
+          throw TypeError
+          }
+          if (a === undefined || b === undefined || type === undefined) {
+              throw TypeError
+          }
+         
+      
+      if (type === 'SUM') {
+        return Math.round(a) + Math.round(b)
+      }
+        if (type === 'SUBTRACT') {
+            return Math.round(a) - Math.round(b)
+        }   
+        if (type === 'DIVIDE') {
+            if (Math.round(b) === 0) {
+                return 'Error'
+            }
+            return Math.round(a) / Math.round(b)
+        }
+    
+    }
+}
+module.exports = Utils;
