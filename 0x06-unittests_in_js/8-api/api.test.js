1166b1ab789dd5d0cc803a9aa2f9ce830afe8309
AbdiAbader <abdiprocode@gmail.com>
2023-05-18 22:49:56 +0300

    api - 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('./api');
+
+describe('Index Page', () => {
+  it('returns correct status code', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+
+  it('returns correct result', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Welcome to the payment system');
+  });
+ 
+});
