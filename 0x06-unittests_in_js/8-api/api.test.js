2ed9868e77d21c8e0f06673bc4d2217921751672
AbdiAbader <abdiprocode@gmail.com>
2023-05-18 22:57:42 +0300

    api - 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,17 @@
 const request = require('supertest');
 const app = require('./api');
 
-describe('Index Page', () => {
-  it('returns correct status code', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-  });
-
-  it('returns correct result', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toBe('Welcome to the payment system');
-  });
-  it('returns incorrect result', async () => {
-    const response = await request(app).get('/a');
-    expect(response.status).toBe(404);
+describe('Index Page TEst', () => {
+    it('test GET /', (done) => {
+        request(app)
+        .get('/')
+        .expect(200, 'Welcome to the payment system')
+        .end((err, res) => {
+            if (err) throw err;
+            done();
+        });
     });
-});
+    }
+);
+
     
