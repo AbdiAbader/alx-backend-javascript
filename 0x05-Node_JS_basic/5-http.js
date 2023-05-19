4d2bf11ecf909891e096f115c69eaea863836120
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 14:19:28 +0300

    task 3 and task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,7 +9,7 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n\n');
+    res.write('This is the list of our students\n');
 
     countStudents(databaseFilePath)
       .then((data) => {
@@ -19,9 +19,6 @@ const app = http.createServer((req, res) => {
       .catch((error) => {
         res.end(error.message);
       });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
   }
 });
 
