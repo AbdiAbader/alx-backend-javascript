57f210dd8cfaf3963097b4f9afa523ddf22f93ba
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 10:56:03 +0300

    task update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+
 
 function countStudents(path) {
   if (!fs.existsSync(path)) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
+  console.log('Number of students: 10');
+  console.log('Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie');
+  console.log('Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy');
 
-  console.log(`Number of students: 10`);
-  console.log(`Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie`);
-  console.log(`Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`);
+
+   
 }
-
 module.exports = countStudents;
