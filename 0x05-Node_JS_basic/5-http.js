43e8904fe4183ebc8d59d41f383bc5a5ae44daae
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 14:17:36 +0300

    task 3 and task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,13 +4,12 @@ const countStudents = require('./3-read_file_async');
 const databaseFilePath = process.argv[2];
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
+    res.write('This is the list of our students\n\n');
 
     countStudents(databaseFilePath)
       .then((data) => {
@@ -20,12 +19,14 @@ const app = http.createServer((req, res) => {
       .catch((error) => {
         res.end(error.message);
       });
-  } 
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
 });
 
 app.listen(1245, () => {
-  console.log('Server listening on port 1245:127.0.0.1');
-  
+  console.log('Server listening on port 1245: 127.0.0.1');
 });
 
 module.exports = app;
