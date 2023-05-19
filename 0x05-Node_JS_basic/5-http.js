3d1f521f0381f650b043e62f375a1428d1f08c6f
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 14:21:44 +0300

    task 3 and task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -13,7 +13,9 @@ const app = http.createServer((req, res) => {
 
     countStudents(databaseFilePath)
       .then((data) => {
-        res.write(data);
+        res.write(`Number of students: ${data.students.length}\n`);
+        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
         res.end();
       })
       .catch((error) => {
