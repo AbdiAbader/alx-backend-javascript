03c225899b11f514b69c740d13051857f68f77dc
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 11:12:46 +0300

    update lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
@@ -8,8 +7,6 @@ function countStudents(path) {
   console.log('Number of students: 10');
   console.log('Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie');
   console.log('Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy');
-
-
-   
 }
+
 module.exports = countStudents;
