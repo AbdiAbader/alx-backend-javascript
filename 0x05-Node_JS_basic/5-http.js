745cf96a65c54da5ad98b311a9769abfa95b0352
AbdiAbader <abdiprocode@gmail.com>
2023-05-19 13:53:56 +0300

    task 5 update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -12,35 +12,12 @@ const app = http.createServer((req, res) => {
     res.write('This is the list of our students\n\n');
 
     countStudents(databaseFilePath)
-
-        .then(() => {
-          const data = fs.readFileSync(databaseFilePath, 'utf8');
-          const students = data.split('\n');
-          const totalStudents = students.length - 2;
-          const studentsListCS = [];
-          const studentsListSWE = [];
-      
-          students.forEach((student) => {
-            const fields = student.split(',');
-            if (fields.length === 4) {
-              const field = fields[3].trim();
-              if (field === 'CS') {
-                studentsListCS.push(fields[0]);
-              } else if (field === 'SWE') {
-                studentsListSWE.push(fields[0]);
-              }
-            }
-          });
-          res.write(`Number of students: ${totalStudents}\n`);
-          res.write(`Number of students in CS: ${studentsListCS.length}. List: ${studentsListCS.join(', ')}\n`);
-          res.write(`Number of students in SWE: ${studentsListSWE.length}. List: ${studentsListSWE.join(', ')}\n`);
-          res.end();
-        })
-        .catch((error) => {
-          res.end(error.message);
-        });
-      
-    
+      .then((data) => {
+        res.write(data);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found');
